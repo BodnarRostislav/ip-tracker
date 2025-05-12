5a78e0cc6f5d6b3aeb27049da864eda5c4d49712
BodnarRostislav <8171767@gmail.com>
2025-05-12 12:02:05 +0300

    app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request
 
 app = Flask(__name__)
@@ -8,4 +9,5 @@ def get_ip():
     return f"<h2>Ваша IP-адреса: {user_ip}</h2>"
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
